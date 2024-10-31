afc2698df0622498b9b3509a7efb307e9dfde82b
GustavoHenriqueF <162469808+GustavoHenriqueF@users.noreply.github.com>
2024-10-31 09:11:50 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,91 +1,53 @@
-let movies = [
-
-  { title: "Toy Story", age: "livre", description: "Uma aventura animada com brinquedos que ganham vida." },
-
-  { title: "The Lion King", age: "livre", description: "A história épica de Simba, um jovem leão." },
-
-  { title: "Deadpool", age: "18+", description: "Um anti-herói que quebra a quarta parede em sua luta contra vilões." },
-
-  { title: "The Matrix", age: "14+", description: "Uma batalha entre humanos e máquinas em um mundo virtual." },
-
-  { title: "The Shawshank Redemption", age: "16+", description: "A história de um homem injustamente condenado a prisão." },
-
-  { title: "Finding Nemo", age: "livre", description: "Um pai peixe que faz de tudo para encontrar seu filho perdido." },
-
-];
-
-let selectedMovie = {};
+let campoIdade;
+let campoFantasia;
+let campoAventura;
 
 function setup() {
-
-  createCanvas(900, 400);
-
-  let ageInput = createSelect();
-
-  ageInput.position(20, 20);
-
-  ageInput.option('Escolha a faixa etária');
-
-  ageInput.option('livre');
-
-  ageInput.option('14+');
-
-  ageInput.option('16+');
-
-  ageInput.option('18+');
-
-  ageInput.changed(() => {
-
-    selectMovie(ageInput.value());
-
-  });
-
-}
-
-function selectMovie(age) {
-
-  let recommendations = movies.filter(movie => movie.age === age);
-
-  if (recommendations.length > 0) {
-
-    selectedMovie = random(recommendations);
-
-  } else {
-
-    selectedMovie = {};
-
-  }
-
+  createCanvas(850, 500);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("9");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
 }
 
 function draw() {
+  background("#A08EA3");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
 
-  background(220);
+  fill(color(86, 10, 135));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
 
-  
-
-  if (selectedMovie.title) {
-
-    textSize(24);
-
-    text("Recomendação de Filme:", 20, 100);
-
-    
-
-    textSize(20);
-
-    text("Título: " + selectedMovie.title, 20, 140);
-
-    text("Classificação: " + selectedMovie.age, 20, 180);
-
-    text("Descrição: " + selectedMovie.description, 20, 220);
-
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Ladrões de bicicleta";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Depois da chuva";
+        }
+      }
+    }
   } else {
-
-    textSize(20);
-
-    text("Escolha uma faixa etária para ver recomendações!", 20, 100);
-
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
+    }
   }
-
 }
